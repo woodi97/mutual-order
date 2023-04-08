470b358b97aa272923f484cb71d7604c859faf0f
woodi97 <woodi.daily@gmail.com>
2023-04-09 03:09:09 +0900

    chore: change pkm(yarn -> pnpm), add changeset, add shared prettier-config

---
+++ apps/queen-web-app/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>something</div>;
+}
+
+export default App;
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,17 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ["react", "react-hooks", "simple-import-sort", "sonarjs"],
+  extends: ["plugin:react/recommended", "next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "no-console": "warn",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
   parserOptions: {
     babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+      presets: [require.resolve("next/babel")]
+    }
+  }
 };
